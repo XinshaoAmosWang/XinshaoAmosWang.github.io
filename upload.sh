632ee217ab778cb9dbe0e25210fcbfa6c8092728
XinshaoAmosWang <xinshaowang@gmail.com>
2021-03-13 19:52:03 +0000

    remove upload.sh

--- upload.sh
+++
@@ -1,7 +0,0 @@
-git remote set-url origin git@github.com:XinshaoAmosWang/XinshaoAmosWang.github.io.git
-
-git add .
-git commit -m "update on cvpr-21 accepted paper: Progressive Self Label Correction (ProSelfLC) for Training Robust Deep Neural Networks"
-git push origin master
-
-git pull origin master 
